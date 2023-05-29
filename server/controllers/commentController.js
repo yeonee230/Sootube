dcb162e388fb4a7c785abafac2a67d0f57a9f199
SUYEON KIM <yeonee230@gmail.com>
2023-05-29 16:41:54 +0900

    server commnent

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -3,7 +3,7 @@ import Comment from '../models/Comment';
 import Video from '../models/Video';
 
 export const addComment = async (req, res, next) => {
-  const newComment = new Comment({ ...req.body, userId: req.user.id });
+  const newComment = new Comment({ ...req.body, userId: req.user._id });
   try {
     const savedComment = await newComment.save();
     res.status(200).send(savedComment);
