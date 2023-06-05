fcc43d6d94052c44120a10d27886a825d3725b0d
SUYEON KIM <yeonee230@gmail.com>
2023-06-05 22:26:22 +0900

    fix verifyToken

--- server/util/verifyToken.js
+++ server/util/verifyToken.js
@@ -1,16 +1,31 @@
 import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) return next(createError(401, "You are not authenticated!"));
+// export const verifyToken = (req, res, next) => {
+//   const token = req.cookies.access_token;
+//   if (!token) return next(createError(401, "You are not authenticated!"));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
-    req.user = user;
-    next()
-  });
-};
+//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//     if (err) return next(createError(403, "Token is not valid!"));
+//     req.user = user;
+//     next()
+//   });
+// };
+
+export const verifyToken = (req, res, next) => {
+  if(!req.headers.authorization) return res.status(403).json({msg: "Not authorized. No token"})
+
+  if(req.headers.authorization && req.headers.authorization.startsWith("Bearer ")){
+      const token = req.headers.authorization.split(" ")[1]
+      jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+         if(err) return res.status(403).json({msg: "Wrong or expired token"})
+         else {
+            req.user = data // data = {id: newUser._id}
+            next()
+         }
+      })
+  }
+}
 
 
 
