1625ece5fb6a571fd3012792ec8a553205f2a3ff
SUYEON KIM <yeonee230@gmail.com>
2023-06-05 21:45:11 +0900

    react cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,7 +56,7 @@ export const signin = async (req, res, next) => {
         httpOnly: true,
       })
       .status(200)
-      .json(others, token );
+      .json(others);
   } catch (err) {
     next(err);
   }
